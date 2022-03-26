ed978a56a06c2fbac8d3657df72a8ea5838b0cc7
Raúl Ahumada <raulahumada00@gmail.com>
2022-03-26 15:26:16 -0300

    First commit

---
+++ src/components/layout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import Navigation from '../components/navigation';
+import 'prismjs/themes/prism-okaidia.css';
+
+export default ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  );
+  return (
+    <div className="site-wrapper">
+      <header className="site-header">
+        <div className="site-title">
+          <Link to="/">RAÚL AHUMADA</Link>
+        </div>
+        <Navigation />
+      </header>
+      {children}
+      <footer className="site-footer">
+        <p>
+          &copy; {new Date().getFullYear()} Raúl Ahumada &bull; Hecho a mano con{' '}
+          <span role="img" aria-label="love">
+            ❤️
+          </span>
+        </p>
+      </footer>
+    </div>
+  );
+};
---
+++ src/components/navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import ThemeChanger from '../components/themeChanger';
+
+export default (props) => (
+  <nav className="navigation">
+    <Link to="/contact">Contacto</Link>
+    <ThemeChanger />
+  </nav>
+);
